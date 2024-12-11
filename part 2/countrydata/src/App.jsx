c92213f75936a22ba9599de2ce7ca9c49bf184f0
XinnyDonut <xinxin.wang0703@gmail.com>
2024-12-11 13:34:53 +1300

    Finish countrydata 2.18

---
+++ part 2/countrydata/src/App.jsx
@@ -0,0 +1,92 @@
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+import './App.css'
+
+const Search=({value,onChange})=>
+  <div>
+    find countries <input value={value} onChange={onChange} />
+  </div>
+
+const Result=({array,country})=>{
+  if(array.length>10){
+    return <p>Too many matches,specify another filter</p>
+  }else if(array.length<10&&array.length>1){
+    return <ul>{array.map((x,i)=><li key={i}>{x.name.common}</li>)}</ul>
+  }
+}
+const Info=({country})=>{
+  if(!country){
+    return null
+  }
+  else{
+    return <>
+    <h2>{country[0]}</h2>
+    <p>capital: {country[1]}</p>
+    <p>area: {country[2]}</p>
+    <h3>Languages</h3>
+    <ul>
+     {country[3].map(x=><li key={x}>{x}</li>)}
+    </ul>
+    <img src={country[4]}/>
+  </>
+  }
+  
+}
+
+function App() {
+  console.log("re-rendering..");
+
+  const [input,setInput]=useState("")
+  const [result,setResult]=useState([])
+  const [country,setCountry]=useState(null)
+
+  useEffect(()=>{
+    if(input!==""){
+      console.log("fetching country data...")
+      
+      axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+   
+      .then(response=> 
+        setResult(response.data.filter(x=>
+            x.name.common.toLowerCase().includes(input.toLowerCase()))) 
+      )  
+      .catch(error => {
+        console.error("Error fetching data:", error)
+        setInput("")
+      })
+    }
+  },[input])
+
+  useEffect(()=>{
+    if(result.length==1){
+      console.log("finally one country is out")
+      axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${result[0].name.common.toLowerCase()}`)
+      .then(response=>{
+        const obj=response.data
+        const objArray=[]
+        objArray.push(obj.name.common,obj.capital[0],obj.area.toString(),Object.values(obj.languages),obj.flags.png)
+        console.log(objArray);
+        setCountry(objArray)
+      })
+      setInput("")
+    }
+  },[result[0]])
+  
+  
+  const handleChange=(event)=>{
+    setInput(event.target.value)
+  }
+ 
+  return (
+    <>
+      <Search value={input} onChange={handleChange}/>
+      <Result array={result}/>
+      <Info country={country}/>
+      
+    </>
+  )
+}
+
+export default App
--- part 2/phonebook/src/App.jsx
+++ part 2/phonebook/src/App.jsx
@@ -2,7 +2,22 @@ import { useState,useEffect } from 'react'
 import personsService from './service/persons' 
 
 const Person=({array,handler})=>array.map(p=><p key={p.id}>{p.name} {p.number}<button onClick={()=>handler(p.id)}>delete</button></p>)
-
+const Notification=({text,style})=>{
+  const notifStyle={
+    color:'red',
+    backGround:'lightgrey',
+    fontSize:20,
+    borderStyle:'solid',
+    borderRadius:5,
+    padding:10,
+    marginBottom:10,
+  }
+  if(text===null){
+    return null
+  }else{
+    return <div style={notifStyle}>{text}</div>
+  }  
+}
 const Search=({searchName,handleSearchChange})=>(
   <div>filter shown with
       <input
@@ -41,6 +56,7 @@ const App = () => {
   const [newName, setNewName] = useState('')
   const [newNumber,setNewNumber]=useState('')
   const [searchName,setSearchName]=useState('')
+  const [notif,setNotif]=useState(null)
 
   useEffect(()=>{
     personsService
@@ -61,9 +77,14 @@ const App = () => {
       const confirmed=window.confirm(`${personObj.name} is already added to the book, replace old number with the new one?` )
       if(confirmed){
         const existPerson=persons.find(x=>x.name===personObj.name)
+        const name=existPerson.name
         personsService
           .update(existPerson.id,{...existPerson,number:newNumber})
           .then(returnedPerson=>setPersons(persons.map(x=>x.id===existPerson.id?returnedPerson:x)))
+          .catch(error=>{
+            setNotif(`${existPerson.name} has already been removed from the server`)
+            setPersons(persons.filter(x => x.id !== existPerson.id)); 
+          })
         setNewName("")
         setNewNumber('')
       }
@@ -71,10 +92,16 @@ const App = () => {
     }else{  
       personsService
         .add(personObj)
-        .then(returnedPerson=>setPersons(persons.concat(returnedPerson)))
-          
+        .then(returnedPerson=>{
+          setPersons(persons.concat(returnedPerson))
+          setNotif(`Added ${returnedPerson.name}`)
+          setTimeout(()=>{
+            setNotif(null)
+          },5000)
+        }) 
       setNewName("")
       setNewNumber("")
+      
     }
     }  
   
@@ -110,6 +137,7 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification text={notif}/>
       <Search searchName={searchName} handleSearchChange={handleSearchChange}/>
       <h3>Add a new</h3>
       <PersonForm addPerson={addPerson} newName={newName} handleNameChange={handleNameChange}
